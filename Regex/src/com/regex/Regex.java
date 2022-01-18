358428ac218c180704894b4ae7875abfc334e2fc
atul080 <atulsrivastav16k26@gmaial.com>
2022-01-18 07:14:18 +0530

    Added: email validation functionality added

--- Regex/src/com/regex/Regex.java
+++ Regex/src/com/regex/Regex.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static Pattern p= Pattern.compile("[A-Z]{1}[a-z]{2,}");
+    public static Pattern p= Pattern.compile("([a-z]|[a-z+-.a-z0-9])+((@[^-]+[a-z+.a-z+.a-z])|(@[^-][a-z+.a-z]))");
     public static void main(String[] ar)
     {
         while(true) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the valid last name.");
+            System.out.println("Enter the valid email id.");
             String input = sc.next();
             boolean check = p.matcher(input).matches();
             if (check)
-                System.out.println("Last Name is valid.");
+                System.out.println("Email id is valid.");
             else
-                System.out.println("Invalid Last Name");
+                System.out.println("Invalid Email.");
         }
     }
 }
