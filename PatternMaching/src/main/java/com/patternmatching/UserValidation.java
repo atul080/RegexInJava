d99dc45bb52bd6dad4e32d6fe70c14c28524c421
atul080 <atulsrivastav16k26@gmaial.com>
2022-01-18 07:44:24 +0530

    Added: Junit test cases for user validation program

--- PatternMaching/src/main/java/com/patternmatching/UserValidation.java
+++ PatternMaching/src/main/java/com/patternmatching/UserValidation.java
@@ -8,8 +8,59 @@ package com.patternmatching;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-
-    public void msg() {
-        System.out.println("Welcome to user validation");
-		}
+    /*
+    * Method to validate first name.
+    * @condition- Name should contain min 1 Capital and 2 small alphabets
+    * @param STRING
+    * @return BOOLEAN
+    * */
+    public boolean checkFirstName(String firstName) {
+        Pattern p= Pattern.compile("[A-Z]{1}[a-z]{2,}");
+        boolean check = p.matcher(firstName).matches();
+        return check;
+    }
+    /*
+     * Method to validate Last name.
+     * @condition- Name should contain min 1 Capital and 2 small alphabets
+     * @param STRING
+     * @return BOOLEAN
+     * */
+    public boolean checkLastName(String lastName) {
+        Pattern p= Pattern.compile("[A-Z]{1}[a-z]{2,}");
+        boolean check = p.matcher(lastName).matches();
+        return check;
+    }
+    /*
+     * Method to validate phone number.
+     * @condition- Phone number should contain contry code followed by 10 dit number ex- 91 4535756789
+     * @param STRING
+     * @return BOOLEAN
+     * */
+    public boolean checkPhoneNumber(String phnNum) {
+        Pattern p= Pattern.compile("^[0-9]{2}\\s[0-9]{10}$");
+        boolean check = p.matcher(phnNum).matches();
+        return check;
+    }
+    /*
+     * Method to validate email id.
+     * @condition- Email should contain '@' and follow all the given conditions in question
+     * @param STRING
+     * @return BOOLEAN
+     * */
+    public boolean checkEmail(String email) {
+        Pattern p= Pattern.compile("([a-z]|[a-z+-.a-z0-9])+((@[^-]+[a-z+.a-z+.a-z])|(@[^-][a-z+.a-z]))");
+        boolean check = p.matcher(email).matches();
+        return check;
+    }
+    /*
+     * Method to validate password
+     * @condition-  password should be of min 8 char, should contain min 1 capital, 1 digit, 1 special symbol
+     * @param STRING
+     * @return BOOLEAN
+     * */
+    public boolean checkPassword(String pswd) {
+        Pattern p= Pattern.compile("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,}");
+        boolean check = p.matcher(pswd).matches();
+        return check;
+    }
 }
--- PatternMaching/src/test/java/com/patternmatching/UserValidationTest.java
+++ PatternMaching/src/test/java/com/patternmatching/UserValidationTest.java
@@ -6,5 +6,96 @@ import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 
 public class UserValidationTest {
-    
+    UserValidation uv=new UserValidation();
+    /*
+    * Test case to check VALID first name.
+    * @calls checkFirstName method of UserValidation class
+    * */
+    @Test
+    public void fitstName_ifProper_shouldReturnTrue()
+    {
+        boolean check=uv.checkFirstName("Rahul");
+        Assertions.assertTrue(check);
+    }
+    /*
+     * Test case to check INVALID first name.
+     * @calls checkFirstName method of UserValidation class
+     * */
+    @Test
+    public void fitstName_ifNotProper_shouldReturnFalse()
+    {
+        boolean check=uv.checkFirstName("rahul");
+        Assertions.assertFalse(check);
+    }
+    /*
+     * Test case to check VALID Last name.
+     * @calls checkLastName method of UserValidation class
+     * */
+    @Test
+    public void lastName_ifProper_shouldReturnTrue()
+    {
+        boolean check=uv.checkLastName("Kumar");
+        Assertions.assertTrue(check);
+    }
+    /*
+     * Test case to check VALID phone number.
+     * @calls checkPhoneNumber method of UserValidation class
+     * */
+    @Test
+    public void phoneNumber_ifProper_shouldReturnTrue()
+    {
+        boolean check=uv.checkPhoneNumber("91 1235845678");
+        Assertions.assertTrue(check);
+    }
+    /*
+     * Test case to check INVALID phone number.
+     * @calls checkPhoneNumber method of UserValidation class
+     * */
+    @Test
+    public void phoneNumber_ifNotProper_shouldReturnFalse()
+    {
+        boolean check=uv.checkPhoneNumber("911235845678");
+        Assertions.assertFalse(check);
+    }
+    /*
+     * Test case to check VALID email id.
+     * @calls checkEmail method of UserValidation class
+     * */
+    @Test
+    public void email_ifProper_shouldReturnTrue()
+    {
+        boolean check=uv.checkEmail("abc+100@gmail.com");
+        Assertions.assertTrue(check);
+    }
+    /*
+     * Test case to check INVALID email id.
+     * @calls checkEmail method of UserValidation class
+     * */
+    @Test
+    public void email_ifNotProper_shouldReturnFalse()
+    {
+        boolean check=uv.checkEmail("abc+100gmail.com");
+        Assertions.assertFalse(check);
+    }
+    /*
+     * Test case to check VALID password.
+     * @calls checkPassword method of UserValidation class
+     * */
+    @Test
+    public void password_ifProper_shouldReturnTrue()
+    {
+        boolean check=uv.checkPassword("Abc6@ghjkloui");
+        Assertions.assertTrue(check);
+    }
+    /*
+     * Test case to check INVALID password.
+     * @calls checkPassword method of UserValidation class
+     * */
+    @Test
+    public void password_ifNotProper_shouldReturnFalse()
+    {
+        boolean check=uv.checkPassword("abc@ghjkloui");
+        Assertions.assertFalse(check);
+    }
+
 }
