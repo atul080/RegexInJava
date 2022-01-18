3e79579df0e5f4a1dd4baf79715724c945cd6537
atul080 <atulsrivastav16k26@gmaial.com>
2022-01-18 07:38:57 +0530

    Added: Junit test cases for user validation program

---
+++ PatternMaching/src/main/java/com/patternmatching/UserValidation.java
@@ -0,0 +1,15 @@
+/*
+* @Author- Atul Srivastav
+* @date 16/01/22
+* User data validation program
+* */
+package com.patternmatching;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+
+    public void msg() {
+        System.out.println("Welcome to user validation");
+		}
+}
---
+++ PatternMaching/src/test/java/com/patternmatching/UserValidationTest.java
@@ -0,0 +1,10 @@
+package com.patternmatching;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class UserValidationTest {
+    
+}
