707304b4823a7813333707ceec034c701ae93c04
atul080 <atulsrivastav16k26@gmaial.com>
2022-01-18 07:26:52 +0530

    Added: All sample email validation program

--- Regex/src/com/regex/Regex.java
+++ Regex/src/com/regex/Regex.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static Pattern p= Pattern.compile("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,}");
+    public static Pattern p= Pattern.compile("([a-z]|[a-z+-.a-z0-9])+((@[^-]+[a-z+.a-z+.a-z])|(@[^-][a-z+.a-z]))");
     public static void main(String[] ar)
     {
-        while(true) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the valid password.");
-            String input = sc.next();
-            boolean check = p.matcher(input).matches();
-            if (check)
-                System.out.println("Password is valid.");
-            else
-                System.out.println("Invalid Password.");
+        String[] emails={"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
+            for(String id:emails)
+            {
+                boolean check = p.matcher(id).matches();
+             if (check)
+                System.out.println(id+" Email id is valid.");
+             else
+                System.out.println(id+" Invalid Email.");
         }
     }
 }
